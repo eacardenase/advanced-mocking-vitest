2109aaf5f847427cb89d35aceb0e97d56cf69d88
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-10-11 20:06:33 -0500

    Initial setup for dom/browser testing

---
+++ util/dom.test.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+import { it, vi } from 'vitest';
+import { Window } from 'happy-dom';
+
+import { showError } from './dom';
+
+const htmlDocPath = path.join(process.cwd(), 'index.html');
+const htmlDocumentContent = fs.readFileSync(htmlDocPath, 'utf8');
+
+const window = new Window();
+const document = window.document;
+document.write(htmlDocumentContent);
+
+vi.stubGlobal('document', document);
+
+console.log(htmlDocumentContent);
+
+it('first test', () => {
+    showError('test');
+});
